223329d7a2ef9ee9b7ff003cc651099dad36a763
Kevin Mahoney <kevin.mahoney@maplecroft.com>
2011-12-15 11:43:25 +0000

    Version bump: 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='drel',
-    version='0.0.1',
+    version='0.0.2',
     author='Kevin Mahoney',
     author_email='git@kevinmahoney.co.uk',
     packages=['drel'],
