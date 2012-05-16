17acfbbc79e2a23ddd219da200a57aaf9e5309b9
Kevin Mahoney <kevin.mahoney@maplecroft.com>
2012-05-16 11:33:39 +0100

    Version 0.0.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='drel',
-    version='0.0.2',
+    version='0.0.3',
     author='Kevin Mahoney',
     author_email='git@kevinmahoney.co.uk',
     packages=['drel'],
